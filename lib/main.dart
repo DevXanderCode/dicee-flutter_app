0ae3cc9fa9ffa5b7f65eaf5b6300efac95ad46a6
Alex Chinedu <nwekealexchinedu@gmail.com>
2022-01-09 06:10:06 +0100

    placed the dice images inside a text button widget

--- lib/main.dart
+++ lib/main.dart
@@ -22,14 +22,18 @@ class DicePage extends StatelessWidget {
       child: Row(
         children: <Widget>[
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
+            child: TextButton(
+              onPressed: () {
+                print("Left image got Pressed");
+              },
               child: Image.asset("images/dice1.png"),
             ),
           ),
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
+            child: TextButton(
+              onPressed: () {
+                print("Right image got pressed");
+              },
               child: Image.asset("images/dice2.png"),
             ),
           )
